075ddb3b2adee92ea60e84683a74967a3d76a889
HariHaranS30 <54067532+HariHaranS30@users.noreply.github.com>
2019-08-14 19:12:52 +0530

    Create countofdigits.java

---
+++ countofdigits.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+public class countofdigits
+{
+ public static void main(String args[])
+ {
+  int no,count=0;
+  Scanner in = new Scanner(System.in);
+  no = in.nextInt();
+  for(; num!=0; num/=10; count++)
+  System.out.println(count);
+ }
+ }
