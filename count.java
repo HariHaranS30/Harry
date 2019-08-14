83f3bb9f3ea6a2640c071d63a53cff1ffcd0a98c
HariHaranS30 <54067532+HariHaranS30@users.noreply.github.com>
2019-08-14 20:54:40 +0530

    Update and rename countofdigits.java to count.java

--- count.java
+++ count.java
@@ -1,14 +1,16 @@
 import java.util.*;
 import java.lang.*;
-public class countofdigits
+public class count
 {
  public static void main(String args[])
  {
   int no,count=0;
   Scanner in = new Scanner(System.in);
   no = in.nextInt();
-  for(n0!=0; n0/=10;count++)
+  while(no!=0)
   {
+   no/=10;
+   ++count;
   }
   System.out.println(count);
  }
