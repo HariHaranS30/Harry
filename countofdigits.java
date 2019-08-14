4a372596cc9fd3e62f22c8c0fd1dbfc68adbab61
HariHaranS30 <54067532+HariHaranS30@users.noreply.github.com>
2019-08-14 19:19:28 +0530

    Update countofdigits.java

--- countofdigits.java
+++ countofdigits.java
@@ -7,7 +7,7 @@ public class countofdigits
   int no,count=0;
   Scanner in = new Scanner(System.in);
   no = in.nextInt();
-  for(; n0!=0; n0/=10,count++)
+  for(n0!=0; n0/=10;count++)
   {
   }
   System.out.println(count);
