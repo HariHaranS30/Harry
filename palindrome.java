b0906d514807520084acd61f8f33eb22e269d0d2
HariHaranS30 <54067532+HariHaranS30@users.noreply.github.com>
2019-08-14 21:27:32 +0530

    Create palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+public class palindrome
+{
+  public static void main(String args[])
+  {
+    int number,rev=0,rem,temp;
+    Scanner in = new Scanner(System.in);
+    number = in.nextInt();
+    temp = number;
+    while(temp!=0)
+    {
+      fem = temp%10;
+      rev = (rev*10)+rem;
+      temp = temp/10;
+     }
+    if (number==rev)
+     System.out.println("yes");
+     else
+     System.out.println("no");
+   }
+ }  
+      
+      
