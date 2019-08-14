0da4000dded9871935de75595c37a8648ffe3cf9
HariHaranS30 <54067532+HariHaranS30@users.noreply.github.com>
2019-08-14 19:02:59 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+public class sum
+{
+ public static void main(String args[])
+ {
+  int n,k,sum=0;
+  Scanner in = new Scanner(System.in);
+  n = in.nextInt();
+  k = in.nextInt();
+  int arr[] = new int[n];
+  for(int i=0;i<n;i++)
+  { 
+   arr[i]=in.nextInt();
+  }
+  for(int j=0;j<k;j++)
+  {
+   sum+=arr[j];
+  }
+ }
+}
