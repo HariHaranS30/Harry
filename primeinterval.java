1b42b2933be747af513c8f144f199a978a7d28ff
HariHaranS30 <54067532+HariHaranS30@users.noreply.github.com>
2019-08-15 10:01:50 +0530

    Create primeinterval.java

---
+++ primeinterval.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+public class primeinterva
+{
+ public static void main(String args[])
+ {
+   int no1,no2,temp=-0;
+   Scanner in = new Scanner(System.in);
+   no1 = in.nextInt();
+   no2 = in.nextInt();
+   for(int 1=no1+1;i<no2;i++)
+   {
+    for(int j=2;j<i-1;j++)
+    {
+      if(i%j==0)
+       temp = temp+1;
+    }
+    if(temp==0)
+     System.out.println(i+" ");
+    else 
+     temp=0;
+   }
+   }
+   }
