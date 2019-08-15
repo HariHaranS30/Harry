6729654169d49f4cf9963fa43f688d923e7456a5
HariHaranS30 <54067532+HariHaranS30@users.noreply.github.com>
2019-08-15 16:44:33 +0530

    Create armstrongs.java

---
+++ armstrongs.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+class armstrongs
+{
+ public static void main(String args[])
+ {
+  int n1,n2,rem,sum=0;
+  Scanner in = new Scanner(System.in);
+  n1 = in.nextInt();
+  n2 = in.nextInt();
+  for(int i=n1+1;i<n2;i++)
+  {
+   while(i!=0)
+   {
+    rem = i%10;
+    sum+=(i*i*i);
+    i/=10;
+   }
+   if(sum==i)
+   System.out.print(i+" ");
+  }
+  }
+  }
