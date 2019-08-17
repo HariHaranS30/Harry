9d533125729dbf3e23fd5b33bd857111840d99ff
HariHaranS30 <54067532+HariHaranS30@users.noreply.github.com>
2019-08-17 10:09:35 +0530

    Create sumofap.java

---
+++ sumofap.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+import java.util.*;
+public class sumofap
+{
+ public static void main(String args[])
+ {
+  int n,a,d,sum;
+  Scanner in = new Scanner(System.in);
+  n = in.nextInt();
+  a = in.nextInt();
+  d = in.nextInt();
+  sum = (n/2)*((2*a)+(n-1)*d);
+  System.out.println(sum);
+  }
+  }
