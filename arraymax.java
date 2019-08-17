2ed79ce7a15ecb1655243308fbbad1a3346c23aa
HariHaranS30 <54067532+HariHaranS30@users.noreply.github.com>
2019-08-17 10:27:37 +0530

    Create arraymax.java

---
+++ arraymax.java
@@ -0,0 +1,23 @@
+import java.lang.*;
+import java.util.*;
+public class arraymax
+{
+ public static void main(String args[])
+ {
+  int n;
+  Scanner in = new Scanner(System.in);
+  n = in.nextInt();
+  int arr[] = new int[n];
+  for(int i=0;i<n;i++)
+  {
+   arr[i]=in.nextInt();
+  }
+  int max=arr[0];
+  for(int j=0;j<n;j++)
+  { 
+   if(arr[j]>max)
+   max = a[j];
+  }
+   System.out.println(max);
+ }
+}
