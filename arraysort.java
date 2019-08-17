39fc1b8b0dbe823fd7ad099a2f653988d62bbe5a
HariHaranS30 <54067532+HariHaranS30@users.noreply.github.com>
2019-08-17 10:43:51 +0530

    Create arraysort.java

---
+++ arraysort.java
@@ -0,0 +1,21 @@
+import java.lang.*;
+import java.util.*;
+public class arraysort
+{
+ public static void main[String args[])
+ {
+  int n;
+  Scanner in = new Scanner(System.in);
+  n = in.nextInt();
+  int a[] = new int[n];
+  for(int i=0;i<n;i++)
+  {
+   a[i]=in.nextInt();
+   }
+   Arrays.sort(a);
+   for(int i=0;i<n;i++)
+   {
+    System.out.println(a[i]);
+    }
+   }
+}
